4958643e3213061d25380eeb243d1828167a8c42
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-03-09 23:22:00 +0100

    temporary height calculations / homepage

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Homepage = () => {
-  return (
-    <section className='min-h-screen'>
-        <h1> Homepage  </h1>
-        <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-            White
-        </button>
 
-        <section>
-            
-        </section>
+  const [remainingHeight, setRemainingHeight] = useState(0);
+
+  useEffect(() => {
+    const navbarHeight = 68;
+    const footerHeight = 56;
+
+    const screenHeight = window.innerHeight;
+    const calculatedHeight = screenHeight - navbarHeight - footerHeight;
+
+    // Set the remaining height state
+    setRemainingHeight(calculatedHeight);
+
+    // Event listener to recalculate height on window resize
+    const handleResize = () => {
+      const newScreenHeight = window.innerHeight;
+      const newCalculatedHeight = newScreenHeight - navbarHeight;
+      setRemainingHeight(newCalculatedHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <section className='flex flex-col items-center justify-center' style={{minHeight: remainingHeight}}>
+        <h1 className='pb-8 text-6xl uppercase bg-gradient-to-r from-secondary to-accent bg-clip-text text-transparent'>Homepage</h1>
+
+        <div className="relative">
+            <input type="text" placeholder="Search" className="input input-bordered input-width pr-16" />
+            <div className="absolute top-0 right-0 bottom-0 flex items-center">
+                <kbd className="kbd flex items-center justify-center w-6 h-1/2 text-sm mr-1">⌘</kbd>
+                <kbd className="kbd flex items-center justify-center w-6 h-1/2 text-sm mr-1">K</kbd>
+            </div>
+        </div>
+
+
     </section>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
