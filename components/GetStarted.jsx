611719b02e652c77bf892f9bfd6f43c7023b732e
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-03-10 21:33:56 +0100

    Logo + footer icon

--- components/GetStarted.jsx
+++ components/GetStarted.jsx
@@ -4,7 +4,7 @@ import Title from './layout/Title'
 
 const GetStarted = () => {
   return (
-    <div className='min-h-screen w-screen bg-base-200'>
+    <div className='min-h-screen max-w-screen bg-base-200'>
         <div className='pt-12'>
             <SectionTitle name="Get started"/>
             <Title headingText="Lets start with DataDepot"/>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-import { FaWarehouse } from "react-icons/fa";
-
+import React from 'react';
+import Link from 'next/link';
+import LogoDark from './layout/LogoDark';
+import LogoLight from './layout/LogoLight';
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-300 sticky top-0">
-        <div className="navbar-start ml-4 ">
-            <FaWarehouse className='h-9 w-7 pr-1'/>
-            <h2 className='text-2xl text-gray-200'> DataDepot </h2>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <ul>
-                <button> <Link className='btn btn-ghost mr-4' href="/auth/login">Login</Link> </button>
-                <button> <Link className='btn btn-primary mr-4' href="/auth/register">Sign in</Link> </button>                
-            </ul>
-        </div>
+      <div className="navbar-start ml-4 ">
+        <LogoLight/>
+        <h2 className='text-2xl pt-2 text-gray-200'> DataDepot </h2>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <ul>
+          <button> <Link className='btn btn-ghost mr-4' href="/auth/login">Login</Link> </button>
+          <button> <Link className='btn btn-primary mr-4' href="/auth/register">Sign in</Link> </button>
+        </ul>
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
