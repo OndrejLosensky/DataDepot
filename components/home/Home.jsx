210410a35054e3cdace6b4e08cf5ec70dbcaf9be
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-03-21 22:12:12 +0100

    new heropage button style

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -12,20 +12,39 @@ const Hero = () => {
           <div class="flex place-items-center before:absolute before:h-[850px] before:w-full sm:before:w-[480px] before:translate-x-[-750px] before:translate-y-[550px] before:rounded-full before:bg-gradient-to-br before:from-warning before:to-transparent before:blur-xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-secondary after:via-success after:blur-3xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-primary before:dark:opacity-20 after:dark:from-green-600 after:dark:via-green-400 after:dark:opacity-100 before:lg:h-[300px] z-[-1]"></div>
 
 
-            <h2 className='pt-32 text-5xl text-center font-semibold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text shadow-glow'>
+            <h2 className='pt-32 text-5xl text-center font-semibold text-white'>
               Store your files efficiently
             </h2>
           
 
 
-            <p className='text-gray-400 text-lg pt-12 text-wrap w-1/4'>
-              DataDepot is the ultimate platform for effortlessly storing and organizing  your files. Have control over it!
+            <p className='text-gray-400 text-lg pt-8 text-wrap w-1/4'>
+              DataDepot is the ultimate platform for effortlessly storing and organizing  your files.
             </p>
-            <button className="mt-8 px-8 py-2 rounded-full border-2 border-blue-500">
-              <span className="flex items-center uppercase font-thin text-gray-400 hover:text-gray-200 duration-300 ">
-                  Get started now!
+
+            <button className="mt-8 px-8 py-2 relative overflow-hidden border-rose-400 text-gray-400 hover:text-gray-200 duration-300 border rounded-full shadow-md hover:shadow-lg"
+              style={{
+                boxShadow: '0 0 6px rgba(214, 125, 235, 0.8)', // Initial glowy pink or purple border
+              }}
+            >
+              <span className="absolute top-0 left-0 w-full h-full rounded-full"></span>
+              <span className="flex items-center uppercase font-thin relative z-10 ">
+                Get started now!
               </span>
             </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
             <p className="text-sm pt-2 font-extralight">
               Available for free
             </p>
