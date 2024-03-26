4282b59755823e97e1e157a5bd8870eb81f0c2da
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-03-26 22:56:15 +0100

    blog page setup / pricing cards page done

--- components/frontend/blog/blog.jsx
+++ components/frontend/blog/blog.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import React from 'react';
 import { FaArrowLeftLong } from "react-icons/fa6";
-import Link from 'next/link'
+import Link from 'next/link';
+import Navbar from '../Navbar';
 
-const blog = () => {
+const Blog = () => {
+  // Array of blog posts
+  const blogPosts = [
+    {
+      heading: 'New Features Added',
+      version: 'v1.2',
+      date: 'March 25, 2024',
+      description: 'Discover the latest features and enhancements in version 1.2 of DataDepot.',
+    },
+    {
+      heading: 'Important Update: Security Patch',
+      version: 'v1.3',
+      date: 'March 26, 2024',
+      description: 'Learn about the crucial security patch released in version 1.3 of DataDepot.',
+    },
+    {
+      heading: 'Enhanced User Experience',
+      version: 'v1.4',
+      date: 'March 27, 2024',
+      description: 'Explore the improvements made to enhance the overall user experience in version 1.4.',
+    },
+    {
+      heading: 'App created',
+      version: 'v1.5',
+      date: 'March 28, 2024',
+      description: 'Explore the realease app',
+    },
+  ];
+
   return (
     <div className='max-w-screen min-h-screen'>
-        <h1 className='text-center py-4'> Blog page</h1>
-        <a><Link className='btn btn-ghost absolute top-0 left-0 ml-4 mt-4' href="/"><FaArrowLeftLong/>Back</Link></a>
+      <div className='w-screen h-16 bg-base-300 flex items-center'>
+        <Link href="/"><button className='btn btn-ghost ml-4'><FaArrowLeftLong /> Back</button></Link>
+      </div>
+       
+      <div className='w-2/4 mx-auto'>
+        <h1 className='text-4xl pl-4 pt-8 text-gray-300'>Welcome to <span className='text-gray-100 font-semibold'>DataDepot blog</span></h1>
+        <p className='ml-4 text-xl pt-2 font-thin'>Here you can find about new versions of the app, features, important changes and more interesting things  </p>
+        <div className='border-b-[0.5px] border-gray-400 mx-4 pt-6'></div>
+        <div className="flex flex-col gap-4 mt-4">
+          {blogPosts.map((post, index) => (
+            <div key={index} className="ml-4 border-[0.5px]  border-gray-500 rounded-lg p-4">
+              <h2 className="text-2xl font-semibold">{post.heading}</h2>
+              <p className="text-gray-500 text-sm">Version: {post.version} - Date: {post.date}</p>
+              <p className="mt-2">{post.description}</p>
+              <button className='px-4 py-1 mt-2 rounded-md shadow-lg bg-blue-500 hover:bg-blue-700 duration-300 text-gray-100'> See more</button>
+            </div>
+          ))}
+        </div>
+      </div> 
     </div>
-  )
-}
+  );
+};
 
-export default blog;
+export default Blog;
--- components/frontend/pricing/Pricing.jsx
+++ components/frontend/pricing/Pricing.jsx
@@ -4,9 +4,89 @@ import Link from 'next/link'
 
 const Pricing = () => {
   return (
-    <div className='max-w-screen min-h-screen'>
-        <h1 className='text-center py-4'> Pricing page</h1>
-        <a><Link className='btn btn-ghost absolute top-0 left-0 ml-4 mt-4' href="/"><FaArrowLeftLong/>Back</Link></a>
+    <div className='max-w-screen min-h-screen bg-[#18191A]'>
+         <div className='w-screen h-16 bg-[#111212] flex items-center'>
+          <Link href="/"><button className='btn btn-ghost ml-4'><FaArrowLeftLong /> Back</button></Link>
+        </div>
+        <div className='flex flex-col items-center mt-16'>
+          <h1 className='text-gray-100 text-4xl font-semibold'> Pricing Plans</h1>
+          <p className='text-md text-gray-300 font-thin'>We currently offer only the <span className='font-semibold'>Free</span> plan.</p>
+        </div>  
+
+        <div className='flex justify-center mt-4 p-4'>
+          <div className='grid grid-cols-3 gap-x-8'>
+            <div className='w-[400px] h-[480px] bg-[#262626] rounded-2xl shadow-xl self-end'>
+              <div className='h-1/3 w-full bg-[#323232] border-b-[0.3px] border-gray-500 shadow-md rounded-t-2xl flex items-end'>
+                  <div className='flex flex-col mb-4'>
+                    <span className='w-7 h-7 ml-8 mb-2  bg-gradient-to-br from-pink-500 to-blue-400 rounded-full'></span>
+                    <h2 className='text-2xl font-bold text-gray-100 ml-8'> Basic </h2>   
+                    <p className='text-md text-gray-400 ml-8 w-2/3'> For more advanced users that want to store large files or big amounts   </p>
+                  </div>
+                </div>
+                <div className='mx-8 mt-10'>
+                  <h2 className='text-3xl text-gray-100'>$5 <span className='text-sm text-gray-400 font-thin'> per month billed annually</span></h2>
+                  <h2 className='font-thin'> or  <span className='text-gray-400 font-semibold'>$9 billed monthly</span> </h2>
+                  <button className='border-[0.5px] w-full mt-6 py-2 border-blue-500 hover:bg-blue-700 duration-300  bg-[#468ced] rounded-lg text-gray-100'> Get Started </button>
+                  <div>
+                    <h3 className='font-semibold text-gray-100 pt-4'> Everything in Free and:</h3>
+                    <ul className='list-disc font-light text-sm pl-4 mt-2 text-gray-200'>
+                      <li className='py-1'>Export option and statistics</li>
+                      <li className='py-1'>500MB capacity for docs</li>
+                      <li className='py-1'>20 color to choose from   </li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+
+             <div className='w-[400px] h-[550px] bg-[#262626] rounded-2xl shadow-xl'>
+      <div className='h-1/3 w-full bg-[#323232] border-b-[0.3px] border-gray-500 shadow-md rounded-t-2xl flex items-end'>
+        <div className='flex flex-col mb-8'>
+          <span className='w-7 h-7 ml-8 mb-4  bg-gradient-to-br from-purple-500 to-green-400 rounded-full'></span>
+          <h2 className='text-2xl font-bold text-gray-100 ml-8'> Free </h2>   
+          <p className='text-md text-gray-400 ml-8 w-2/3'> version for anyone, manage your files efficiently.  </p>
+        </div>
+      </div>
+      <div className='mx-8 mt-16'>
+        <h2 className='text-3xl text-gray-100'>$0 <span className='text-sm text-gray-400 font-thin'> per month billed annually </span></h2>
+        <button className='border-[0.5px] w-full mt-8 py-2 border-gray-200 bg-[#3a3a3a] rounded-lg text-gray-100'> Current </button>
+        <div>
+          <h3 className='font-semibold text-gray-100 pt-8'> Featuring </h3>
+          <ul className='list-disc font-light pl-4 mt-2 text-gray-200'>
+            <li className='py-1'>all files types supported</li>
+            <li className='py-1'>50MB capacity for docs</li>
+            <li className='py-1'>10 different color labels </li>
+          </ul>
+        </div>
+      </div>
+            </div>
+    
+            <div className='w-[400px] h-[480px] bg-[#262626] rounded-2xl shadow-xl self-end'>
+              <div className='h-1/3 w-full bg-[#323232] border-b-[0.3px] border-gray-500 shadow-md rounded-t-2xl flex items-end'>
+                  <div className='flex flex-col mb-4'>
+                    <span className='w-7 h-7 ml-8 mb-2  bg-gradient-to-br from-yellow-300 to-rose-400 rounded-full'></span>
+                    <h2 className='text-2xl font-bold text-gray-100 ml-8'> Pro </h2>   
+                    <p className='text-md text-gray-400 ml-8 w-2/3'> For professionals that like to store ultra large amount of documens  </p>
+                  </div>
+                </div>
+                <div className='mx-8 mt-10'>
+                  <h2 className='text-3xl text-gray-100'>$19 <span className='text-sm text-gray-400 font-thin'> per month billed annually</span></h2>
+                  <h2 className='font-thin'> or  <span className='text-gray-400 font-semibold'>$27 billed monthly</span> </h2>
+                  <button className='border-[0.5px] w-full mt-6 py-2 border-blue-500 hover:bg-blue-700 duration-300  bg-[#468ced] rounded-lg text-gray-100'> Get Started </button>
+                  <div>
+                    <h3 className='font-semibold text-gray-100 pt-4'> Everything in Free, Basic and:</h3>
+                    <ul className='list-disc font-light text-sm pl-4 mt-2 text-gray-200'>
+                      <li className='py-1'>Folder structure system</li>
+                      <li className='py-1'>10GB capacity</li>
+                      <li className='py-1'>Unlimited colors for labels</li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+  </div>
+</div>
+
+
+
     </div>
   )
 }
