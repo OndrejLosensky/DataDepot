a9a19472c6b70cf36002758e494c1618d13e35b9
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-03-28 16:59:02 +0100

    details

--- components/frontend/layout/SectionTitle.jsx
+++ components/frontend/layout/SectionTitle.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const SectionTitle = ({ name }) => {
   return (
     <div className="flex items-center justify-center pb-12">
-      <div className="border-[0.5px] border-gray-500 w-[500px]"></div>
+      <div className="border-[0.7px] border-gray-500 w-[500px]"></div>
       <div className="w-auto mx-12">
-        <p className="text-md font-bold text-center text-gray-300  hover:text-gray-50 duration-300 border border-blue-500 rounded-full py-1 px-4">
+        <p className="text-md font-bold text-center text-gray-400  hover:text-gray-50 duration-300 border border-[#ba4ef0] rounded-full py-1 px-4">
           {name}
         </p>
       </div>
-      <div className="border-[0.5px] border-b-gray-500 w-[500px]"></div>
+      <div className="border-[0.7px] border-gray-500 w-[500px]"></div>
     </div>
   );
 };
