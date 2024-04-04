cf25808e750aef0492f161225ede68bf08c9963d
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-04-04 23:03:49 +0200

    details

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -8,16 +8,13 @@ const Loading = () => {
   const [text] = useTypewriter({
     words: ['Preparing your files...', 'Starting the warehouse...','Loading user settings...','Almost there...','Depot is opening...'],
     loop: true,
-    typeSpeed:70,
-    deleteSpeed:40,
-    delaySpeed:2500,
+    typeSpeed:80,
+    deleteSpeed:30,
+    delaySpeed:1500,
 });
 
   return (
     <div className="max-w-screen min-h-screen flex flex-col justify-center items-center text-white"> {/*  bg-gradient-to-br from-blue-950 opacity-75 to-yellow-950 */}
-      <div className="absolute top-0 left-0 mt-4 ml-4">
-      <Link className='btn btn-ghost' href="/"><FaArrowLeftLong className='text-[#DFDFDF]'/><span className='text-[#DFDFDF]'>Back</span></Link>
-      </div>
 
       <div className='flex flex-row items-center py-4 absolute top-24'>
         <FaWarehouse className={`w-20 mr-2 h-auto text-[#DFDFDF]`}/>
--- progress.md
+++ progress.md
@@ -2,3 +2,7 @@
 #### 4.4. 2024
 - added an registration option to firebase instead of SQLite (not styled correctly)
 - Fixed Enter key working in Login form
+- Added alerts when logining 
+- loading state / button when login successfull
+- Added option to show or hide password
+- removed back button from loading page that opens the **Dashboard**.
