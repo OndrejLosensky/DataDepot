07fb1c8845d997a89f4d46a700e202a4a8aaffa8
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-04-14 20:52:04 +0200

    simplified blog posts page

--- components/frontend/blog/blog.jsx
+++ components/frontend/blog/blog.jsx
@@ -1,104 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import { FaArrowLeftLong } from "react-icons/fa6";
+import { FaArrowLeft } from "react-icons/fa";
 import Link from 'next/link';
 import Footer from '../Footer';
 
 const Blog = () => {
-  const [selectedPost, setSelectedPost] = useState(null);
   const [blogPosts, setBlogPosts] = useState([]);
 
-
   useEffect(() => {
-    fetch('/api/blogPosts')
-      .then(response => response.json())
-      .then(data => setBlogPosts(data))
-      .catch(error => console.error('Error fetching blog posts:', error));
+    fetchBlogPosts();
   }, []);
 
-  const openDialog = (post) => {
-    setSelectedPost(post);
-    document.getElementById('posts').showModal();
+  const fetchBlogPosts = async () => {
+    try {
+      const response = await fetch('/api/blogPosts');
+      if (!response.ok) {
+        throw new Error('Failed to fetch blog posts');
+      }
+      const data = await response.json();
+      setBlogPosts(data);
+    } catch (error) {
+      console.error('Error fetching blog posts:', error);
+    }
   };
 
   return (
-    <div className='max-w-screen min-h-screen overflow-hidden bg-[#18191A]'>
-      <div className='w-screen h-16 bg-[#111212] flex items-center'>
-        <Link href="/"><button className='btn btn-ghost ml-4'><FaArrowLeftLong /> Back</button></Link>
-      </div>
-       
+    <div className='max-w-screen min-h-screen overflow-hidden text-white'>
 
-      <dialog id="posts" className="modal max-w-screen max-h-screen">
-        <div className="h-[90%] mb-48 bg-[#262626] w-2/3 text-left flex flex-col">
-          {selectedPost && (
-            <>
-              <div className=''>
-                <div className='h-12 flex flex-row items-center border-b-[0.3px] border-gray-500 '>
-                  <h2 className='w-64 flex flex-row ml-4'>
-                    <span className='font-thin text-md'>Post number:</span>
-                    <span className='items-center flex pl-1 text-md'>{selectedPost.id}</span>
-                  </h2>
-                  <button className='flex w-full items-center justify-end pr-2' onClick={() => document.getElementById('posts').close()}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="32"
-                      height="32"
-                      viewBox="0 0 48 48"
-                    >
-                      <path
-                        fill="#f44336"
-                        d="M44 24c0 11.045-8.955 20-20 20S4 35.045 4 24 12.955 4 24 4s20 8.955 20 20z"
-                      ></path>
-                      <path
-                        fill="#fff"
-                        d="M29.656 15.516l2.828 2.828-14.14 14.14-2.828-2.828 14.14-14.14z"
-                      ></path>
-                      <path
-                        fill="#fff"
-                        d="M32.484 29.656l-2.828 2.828-14.14-14.14 2.828-2.828 14.14 14.14z"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-                <div className='mx-4 mt-4'>
-                  <h1 className='text-3xl text-[#DFDFDF]'>{selectedPost.heading}</h1>
-                  <div className='flex flex-row'>
-                    <h2 className='font-thin'>Version: {selectedPost.version}</h2>
-                    <p className='mx-2'> – </p>
-                    <h3 className='font-thin'>Date: {selectedPost.date}</h3>
-                  </div>
-                  <p className='pt-2'>{selectedPost.description}</p>
-                  <div className='border-b-[0.5px] border-gray-400 py-2'></div>
-                </div>  
-                {/* Content section */}
-                <div>
+      <div className='w-3/4 mx-auto mt-12'>
+        <Link href="/"><button className='flex flex-row items-center py-2 my-2 hover:bg-gray-700 p-2 rounded-lg duration-300 mb-4'><FaArrowLeft className='mr-2' /> Back</button></Link>
+        <h1 className='text-4xl font-bold mb-4'>Welcome to DataDepot Blog</h1>
+        <p className='text-xl font-light mb-8'>
+          Stay updated with the latest news, updates, and releases. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim mi nec dolor tristique, eget varius elit viverra.
+        </p>
 
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-        <form method="dialog" className="modal-backdrop bg-black opacity-15">
-          <button>close</button>
-        </form>
-      </dialog>
-
-
-      <div className='w-2/4 mx-auto'>
-        <h1 className='text-4xl pl-4 pt-8 text-gray-300'>Welcome to <span className='text-gray-100 font-semibold'>DataDepot blog</span></h1>
-        <p className='ml-4 text-xl pt-2 font-thin'>Here you can find about new versions of the app, features, important changes and more interesting things  </p>
-        <div className='border-b-[0.5px] border-gray-400 mx-4 pt-6'></div>
-        <div className="flex flex-col gap-4 mt-4 mb-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-12">
           {blogPosts.map((post, index) => (
-            <div key={index} className="bg-[#222222] duration-300 hover:drop-shadow-glow mx-4 border-[0.5px]  border-gray-500 rounded-lg p-4">
-              <h2 className="text-2xl font-semibold">{post.heading}</h2>
-              <p className="text-gray-500 text-sm">Version: {post.version} - Date: {post.date}</p>
-              <p className="mt-2">{post.description}</p>
-              <button onClick={() => openDialog(post)} className='px-4 py-1 mt-2 rounded-md shadow-lg bg-blue-500 hover:bg-blue-700 duration-300 text-gray-100'>See more</button>
+            <div key={index} className='bg-[#262626] border border-gray-500 hover:drop-shadow-glow cursor-pointer duration-300 rounded-lg p-4'>
+              <div className="mb-4">
+                <img src="/blog_test.jpeg" alt={`Image ${index+1}`} className='w-full h-[300px] object-cover rounded-md' />
+              </div>
+              <div className='flex flex-row space-x-4 items-center mb-2'>
+                <p className='px-2 py-1 bg-green-200 text-green-600 shadow-md text-xs rounded-lg'> 🎉 New</p>
+                <p className='text-gray-400 text-sm'>Version: {post.version}</p>
+                <p className='text-gray-400 text-sm'>Date: {post.date}</p>
+              </div>
+              <h2 className="text-2xl font-semibold mb-2">{post.heading}</h2>
+              <p className='mb-4'>{post.description}</p>
             </div>
           ))}
         </div>
-      </div> 
-      <Footer/>
+      </div>
+      <Footer />
     </div>
   );
 };
